2b29f3f8dd5d690a769e4db5a7fd3de670ec367c
Danial-Ismail <121781837+Danial-Ismail@users.noreply.github.com>
2023-01-28 20:33:38 +0500

    added index and html files

---
+++ chapter 4/app.js
@@ -0,0 +1,19 @@
+// *****Chapter 4******  //
+
+//Question1
+
+//var name;var product;var quantity;
+
+//Question 2
+
+//legal:var userAlert;var myVar;var name;var userName;var age;
+//illegal:var useralert;var myvar;var user name;var full name;var full-name;
+
+//Question 3
+
+//var heading="Rules for naming JS variables"
+//var varName="Variable name can only contain,numbers,$ and _.For example : $my_1stVariable"
+//var varBegin="Variable must begin with a letter,$ or_.For example : $name,_name or name "
+//var sensitive="Variable names are case sensitive"
+//var keyWords="Variable name should not be JS keywords"
+
