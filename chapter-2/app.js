e61bbfe15f8b96ec2bd94eb4802946e682b2fd42
Danial-Ismail <121781837+Danial-Ismail@users.noreply.github.com>
2023-01-28 20:30:20 +0500

    added index and js files

---
+++ chapter-2/app.js
@@ -0,0 +1,46 @@
+//  ******Chapter 2*****   ////
+
+//Question 1
+
+//var username
+
+//Question 2
+
+//var myName="Danial Ismail";
+
+//Question 3
+
+//var message="Hello World"
+//alert(message);
+
+//Question 4
+
+//var studentName="Jhone Doe"
+//alert(studentName);
+//var age=15
+//alert(age);
+//var education="Certified Mobile Application Development"
+//alert(education);
+
+//Question 5
+
+//var item="Pizza \n Pizz \n Piz \n Pi \n P"
+//alert(item);
+
+//Question 6
+
+//var email="My email address is danialchippa50@gmail.com"
+//alert(email);
+
+//Question 7
+
+//var book="I am trying to learn from the Book A smarter way to learn JavaScript."
+//alert(book);
+
+//Question 8
+
+
+//Question 9
+
+var signs="----------&&&&-----------"
+alert(signs);
