1787be106f616ed9508707b2b040ab454ad706d0
Danial-Ismail <121781837+Danial-Ismail@users.noreply.github.com>
2023-01-28 20:32:18 +0500

    added index and js files

---
+++ chapter-3/app.js
@@ -0,0 +1,22 @@
+// ****** Chapter 3 ******//
+
+//Question 1
+
+//var age="I am 22 years old";
+//alert(age);
+
+//Question 2
+
+//var attendance="you have visited this site 15 times"
+//alert(attendance);
+
+//Question 3
+
+//var birthYear="My birth year is 1996 \n \ndata type of my declared variable is number";
+
+//Question 4
+
+//var name="John Doe"
+//var quantity=5
+//var product="T-shirt(s)"
+//var para="john doe ordered 5 T-shirts(s) on XYZ Clothing store"
